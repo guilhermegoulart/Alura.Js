2ef2cd2364b4633f8cce4ed78efc4aa56c802ffc
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-01-06 16:50:06 -0300

    segundo-dia

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,30 @@
-console.log("fui carregado a partir de um arquivo esterno");
-
 var title = document.querySelector(".titulo");
-
 title.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true; 
+
+if(peso < 0 || peso > 1000) {
+    console.log("Peso Inválido");
+    pesoEhValido = false;
+}
+
+if(altura < 0 || altura > 1000){
+    console.log("Altura Inválida");
+    alturaEhValida = false;
+}
+
+if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
