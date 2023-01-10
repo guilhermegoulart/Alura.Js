bffe5b5e21a8a1d560c2d4351e347105619a9b02
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-01-10 14:50:38 -0300

    Funcao adicionar paciente

--- js/principal.js
+++ js/principal.js
@@ -18,7 +18,7 @@ for (var i=0; i < pacientes.length; i++) {
     var pesoEhValido = true;
     var alturaEhValida = true; 
 
-    if(peso < 0 || peso >= 360) {
+    if(peso < 0 || peso >= 360){
         console.log("Peso Inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
@@ -39,4 +39,41 @@ for (var i=0; i < pacientes.length; i++) {
 
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+    
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td"); 
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = peso / (altura * altura);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+});
 
