5c769b3d9b97f40ff8dbfdaa0040c1c0189e920a
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-01-12 01:21:25 -0300

    refatorando o codigo

---
+++ js/calcula-imc.js
@@ -0,0 +1,49 @@
+var title = document.querySelector(".titulo");
+title.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i=0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true; 
+
+    if(peso < 0 || peso >= 360){
+        console.log("Peso Inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-inválido");
+    }
+
+    if(altura < 0 || altura >= 3.00){
+        console.log("Altura Inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-inválido");
+    }
+
+    if(alturaEhValida && pesoEhValido){
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+
+};
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
